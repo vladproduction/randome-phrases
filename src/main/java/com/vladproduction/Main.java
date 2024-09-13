3895ca99197034682785e23ddc6aaabb49b425c7
vladproduction <vladbogdantsev@gmail.com>
2024-09-13 15:34:24 +0300

    sleep 1 sec

--- src/main/java/com/vladproduction/Main.java
+++ src/main/java/com/vladproduction/Main.java
@@ -25,7 +25,7 @@ public class Main {
         while (true){
             printRandomPhrase();
             try{
-                Thread.sleep(3000);
+                Thread.sleep(1000);
             }catch (InterruptedException e){
                 throw new RuntimeException(e);
             }
